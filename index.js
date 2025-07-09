22646c73dee1c56c4548c6a92160a678c28a15db
Davidm2606 <davidteran2606@hotmail.com>
2025-07-08 23:02:43 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Aplicación DevSecOps funcionando 🚀'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
